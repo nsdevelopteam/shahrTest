01691b029e3e33e4e64f955b42936348069014a6
nssina <nssina@icloud.com>
2020-05-05 04:59:40 -0400

    Build 2
    - LaunchScreen UI finished

---
+++ ShahrYar/Controller/Login/Register/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  ShahrYar
+//
+//  Created by Sina Rabiei on 5/5/20.
+//  Copyright © 2020 Sina Rabiei. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+
+}
